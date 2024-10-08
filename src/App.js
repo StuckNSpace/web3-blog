891d1691d38db2d5b9507aa621ca7953b861ee14
StuckNSpace <34696551+StuckNSpace@users.noreply.github.com>
2024-10-08 15:02:40 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Blog from './pages/Blog';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About Me</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+          </ul>
+        </nav>
+
+        {/* Route Configuration */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to My React App!</h1>
+    </div>
+  );
+}
+
+export default Welcome;
